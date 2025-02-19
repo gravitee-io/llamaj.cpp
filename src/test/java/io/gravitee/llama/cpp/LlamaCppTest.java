60f875f5aa19a33d4f6b28f5bef1a0fc87e7d0f2
remisultan <remi.sultan@gmail.com>
2025-02-19 23:47:54 +0100

    feat: stop string mechanism

--- src/test/java/io/gravitee/llama/cpp/LlamaCppTest.java
+++ src/test/java/io/gravitee/llama/cpp/LlamaCppTest.java
@@ -15,9 +15,17 @@
  */
 package io.gravitee.llama.cpp;
 
+import org.junit.jupiter.api.BeforeAll;
+
+import java.io.IOException;
+import java.lang.foreign.Arena;
+import java.net.URI;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 import static io.gravitee.llama.cpp.llama_h_1.ggml_backend_load_all;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 /**
  * @author Rémi SULTAN (remi.sultan at graviteesource.com)
@@ -27,8 +35,44 @@ abstract class LlamaCppTest {
 
     public static final String NATIVE_LIB = "src/main/resources/libllama.dylib";
 
-    LlamaCppTest() {
+    @BeforeAll
+    public static void beforeAll() throws IOException {
         System.load(Path.of(NATIVE_LIB).toAbsolutePath().toString());
-        ggml_backend_load_all();
+    }
+
+
+    static final String MODEL_TO_DOWNLOAD = "https://huggingface.co/bartowski/Llama-3.2-1B-Instruct-GGUF/resolve/main/Llama-3.2-1B-Instruct-IQ3_M.gguf";
+    static final String MODEL_PATH = "models/Llama-3.2-1B-Instruct-IQ3_M.gguf";
+    static final String SYSTEM = """
+            You are the best at guessing capitals. Respond to the best of your ability. Just answer with the capital.
+            """;
+
+    static Path getModelPath() {
+        try {
+            Path absolutePath = Path.of(MODEL_PATH).toAbsolutePath();
+            if (!Files.exists(absolutePath)) {
+                Files.copy(URI.create(MODEL_TO_DOWNLOAD).toURL().openStream(), absolutePath, REPLACE_EXISTING);
+            }
+            return absolutePath;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static String getPrompt(
+            LlamaModel model,
+            Arena arena,
+            LlamaChatMessages messages,
+            LlamaContextParams contextParams) {
+        var template = new LlamaTemplate(model);
+        return template.applyTemplate(arena, messages, contextParams.nCtx());
+    }
+
+    static LlamaChatMessages builMessages(Arena arena, String system, String input) {
+        return new LlamaChatMessages(arena, List.of(
+                new LlamaChatMessage(arena, Role.SYSTEM, system),
+                new LlamaChatMessage(arena, Role.USER, input)
+        ));
     }
 }
