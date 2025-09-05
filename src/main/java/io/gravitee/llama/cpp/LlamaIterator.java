c8cbc816ac08457e558741f4291b0f368406b2a9
remisultan <remi.sultan@gmail.com>
2025-09-05 17:21:33 +0200

    feat: introduce finish reason

--- src/main/java/io/gravitee/llama/cpp/LlamaIterator.java
+++ src/main/java/io/gravitee/llama/cpp/LlamaIterator.java
@@ -15,10 +15,16 @@
  */
 package io.gravitee.llama.cpp;
 
+import static io.gravitee.llama.cpp.FinishReason.EOS;
+import static java.util.function.Predicate.not;
+
+import io.gravitee.llama.cpp.LlamaTokenizer.TokenizerResponse;
 import java.lang.foreign.Arena;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Spliterator;
 import java.util.Spliterators;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -28,11 +34,97 @@ import java.util.stream.StreamSupport;
  */
 public abstract class LlamaIterator extends ArenaAware implements Iterator<LlamaOutput> {
 
-  protected LlamaIterator(Arena allocator) {
+  protected final LlamaContext context;
+  protected final LlamaSampler sampler;
+  protected final LlamaTokenizer tokenizer;
+
+  protected int maxTokens = -1;
+  protected TokenizerResponse tokenized;
+  protected boolean hasNext;
+
+  protected final AtomicInteger inputTokens = new AtomicInteger(0);
+  protected final AtomicInteger outputTokens = new AtomicInteger(0);
+
+  private List<String> stopStrings = List.of();
+  private String promptMemory = "";
+  protected int maxStopStringSize = 0;
+
+  private FinishReason finishReason;
+
+  public LlamaIterator(Arena allocator, LlamaContext context, LlamaTokenizer tokenizer, LlamaSampler sampler) {
     super(allocator);
+    this.context = context;
+    this.sampler = sampler;
+    this.tokenizer = tokenizer;
   }
 
   public Stream<LlamaOutput> stream() {
     return StreamSupport.stream(Spliterators.spliteratorUnknownSize(this, Spliterator.ORDERED), false);
+  }
+
+  public LlamaIterator initialize(String prompt) {
+    tokenized = tokenizer.tokenize(arena, prompt);
+    inputTokens.set(tokenized.size());
+    hasNext = batch();
+    return this;
+  }
+
+  public abstract boolean batch();
+
+  protected boolean isEog(int tokenId) {
+    boolean isEog = tokenizer.isEog(tokenId);
+    if (isEog) {
+      finishReason = EOS;
+    }
+    return isEog;
+  }
+
+  protected boolean hasNotReachedQuota() {
+    boolean hasNotReachedQuota = maxTokens == -1 || maxTokens > outputTokens.get();
+    if (!hasNotReachedQuota) {
+      finishReason = FinishReason.LENGTH;
+    }
+    return hasNotReachedQuota;
+  }
+
+  public LlamaIterator setMaxTokens(int maxTokens) {
+    this.maxTokens = Math.min(maxTokens, context.nCtx());
+    return this;
+  }
+
+  public int getInputTokens() {
+    return inputTokens.get();
+  }
+
+  public int getOutputTokens() {
+    return outputTokens.get();
+  }
+
+  public FinishReason getFinishReason() {
+    return finishReason;
+  }
+
+  public LlamaIterator setStopStrings(List<String> stopStrings) {
+    this.stopStrings = stopStrings.stream().filter(not(String::isBlank)).toList();
+    maxStopStringSize = this.stopStrings.stream().mapToInt(String::length).max().orElse(0);
+    return this;
+  }
+
+  protected boolean endWithStopString() {
+    boolean endsWithStopString = !(stopStrings.isEmpty() || stopStrings.stream().noneMatch(promptMemory::endsWith));
+    if (endsWithStopString) {
+      finishReason = FinishReason.STOP;
+    }
+    return endsWithStopString;
+  }
+
+  protected void feedPromptMemory(String piece) {
+    if (!stopStrings.isEmpty()) {
+      promptMemory += piece;
+
+      if (promptMemory.length() > maxStopStringSize) {
+        promptMemory = promptMemory.substring(promptMemory.length() - maxStopStringSize);
+      }
+    }
   }
 }
